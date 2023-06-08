b5b5ae2f372ac37ceca7bfa0f310c360009a3495
juan calad <jecalad@outlook.com>
2023-06-07 20:11:19 -0500

    feat: add se backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "juan-calad-cloudcamp-state"  # Specify the name of your S3 bucket
+    key    = "terraform.tfstate"  # Specify the key for your Terraform state file
+    region = "us-east-1"  # Specify the AWS region where the S3 bucket is located
+    encrypt = true  # Optional: Set to true to enable encryption for the state file
+  }
+}
